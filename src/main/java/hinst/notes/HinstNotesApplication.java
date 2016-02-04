0e5a1376d45228733ab49554b99904ba967a57ca
hinst <hinstance@yandex.ru>
2016-02-05 01:28:29 +0300

    initial

---
+++ src/main/java/hinst/notes/HinstNotesApplication.java
@@ -0,0 +1,12 @@
+package hinst.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinstNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HinstNotesApplication.class, args);
+	}
+}
---
+++ src/main/java/hinst/notes/Note.java
@@ -0,0 +1,11 @@
+package hinst.notes;
+
+import javax.persistence.Entity;
+
+@Entity
+class Note {
+	
+	
+	private Long id;
+
+}
