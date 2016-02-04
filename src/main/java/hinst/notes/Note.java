b88891c89b0c118e29ea8a77d90085336ba9e1e2
hinst <hinstance@yandex.ru>
2016-02-05 01:57:48 +0300

    Note

--- src/main/java/hinst/notes/Note.java
+++ src/main/java/hinst/notes/Note.java
@@ -1,11 +1,40 @@
 package hinst.notes;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 class Note {
 	
-	
+	public Note() {
+		super();
+	}
+
+	@Id
+	@GeneratedValue
 	private Long id;
+	private String text;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	@Override
+	public String toString() {
+		return "Note [id=" + id + ", text=" + text + "]";
+	}
 
 }
