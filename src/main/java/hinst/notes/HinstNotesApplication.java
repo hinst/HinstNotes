5712c19eced7db755e52d80f3f79e281b62f299a
hinst <hinstance@yandex.ru>
2016-02-06 19:03:35 +0300

    +h2 dependency. +data.sql

--- src/main/java/hinst/notes/HinstNotesApplication.java
+++ src/main/java/hinst/notes/HinstNotesApplication.java
@@ -9,4 +9,5 @@ public class HinstNotesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HinstNotesApplication.class, args);
 	}
+	
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into note(text) values ('sampleNote1');
+insert into note(text) values ('sampleNote2');
